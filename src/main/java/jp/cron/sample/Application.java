204145f6ac060adf165f3df01af35fa0f0550d97
cron <cron@cron.jp>
2023-08-04 17:40:56 +0900

    Initial commit

---
+++ src/main/java/jp/cron/sample/Application.java
@@ -0,0 +1,28 @@
+package jp.cron.sample;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(Application.class)
+                .web(WebApplicationType.NONE).run(args);
+    }
+
+    @Bean
+    public RestOperations restOperations() {
+        return new RestTemplate();
+    }
+
+//    @Bean
+//    public Gson gson() {
+//        return new Gson();
+//    }
+
+}
